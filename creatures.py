5c7a0778bcbc67d781aff215cb2cd0b63b754b71
DVP-F <carnx@duck.com>
2025-09-22 15:11:51 +0200

    basic ahh data for kreejur

--- creatures.py
+++ creatures.py
@@ -1,5 +1,19 @@
-class Creature:
-    __slots__ = []
+from __future__ import annotations
+import random
 
-    def __init__():
-        0
+class Creature:
+	__slots__ = ["current_string", "iterations", "genes"]
+
+	def __init__(self, genes: dict = {'mutation_rate':0.1, 'aggression':1.0, 'symmetry':2.0, }):
+		self.current_string = ""
+		self.iterations = []
+		self.genes = genes
+		self.mutate()
+
+	def mutate(self):
+		for i in self.genes:
+			for _ in range(max(1, int(self.genes['mutation_rate']))):
+				self.genes[i] += random.choice([random.uniform(0.0, 0.5), random.uniform(-0.5, 0.0)])
+		self.genes['mutation_rate'] = max(0.05, min(self.genes['mutation_rate'], 10.0))
+		self.genes['aggression'] = max(1.0, min(self.genes['aggression'], 10.0))
+		self.genes['symmetry'] = max(0.0, min(self.genes['symmetry'], 5.0))
