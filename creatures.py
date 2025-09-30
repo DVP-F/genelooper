c345c6f9515bafb95e544e81bc8baf14a88a4f78
DVP-F <carnx@duck.com>
2025-09-30 10:38:30 +0200

    i think this is gonna be better yeah

--- creatures.py
+++ creatures.py
@@ -2,6 +2,111 @@
 import random
 
 # noinspection PyTypeChecker
+class Genes:
+	__slots__ = ["genevalues"]
+	genevalues = {
+		'<': { # in principle equal to a chr() string - one character
+			"agr": 0.0,	# Aggression 
+			"str": 0.0,	# Strength 
+			"mut": 0.0, # Mutation 
+			"val": 0.0,	# Value 
+			"sth": 0.0	# Stealth 
+		},
+		'>': {
+			"agr": 0.0,	# Aggression 
+			"str": 0.0,	# Strength 
+			"mut": 0.0, # Mutation 
+			"val": 0.0,	# Value 
+			"sth": 0.0	# Stealth 
+		},
+		'@': {
+			"agr": 0.0,	# Aggression 
+			"str": 0.0,	# Strength 
+			"mut": 0.0, # Mutation 
+			"val": 0.0,	# Value 
+			"sth": 0.0	# Stealth 
+		},
+		'#': {
+			"agr": 0.0,	# Aggression 
+			"str": 0.0,	# Strength 
+			"mut": 0.0, # Mutation 
+			"val": 0.0,	# Value 
+			"sth": 0.0	# Stealth 
+		},
+		'¤': {
+			"agr": 0.0,	# Aggression 
+			"str": 0.0,	# Strength 
+			"mut": 0.0, # Mutation 
+			"val": 0.0,	# Value 
+			"sth": 0.0	# Stealth 
+		},
+		'$': {
+			"agr": 0.0,	# Aggression 
+			"str": 0.0,	# Strength 
+			"mut": 0.0, # Mutation 
+			"val": 0.0,	# Value 
+			"sth": 0.0	# Stealth 
+		},
+		'%': {
+			"agr": 0.0,	# Aggression 
+			"str": 0.0,	# Strength 
+			"mut": 0.0, # Mutation 
+			"val": 0.0,	# Value 
+			"sth": 0.0	# Stealth 
+		},
+		'&': {
+			"agr": 0.0,	# Aggression 
+			"str": 0.0,	# Strength 
+			"mut": 0.0, # Mutation 
+			"val": 0.0,	# Value 
+			"sth": 0.0	# Stealth 
+		},
+		'|': {
+			"agr": 0.0,	# Aggression 
+			"str": 0.0,	# Strength 
+			"mut": 0.0, # Mutation 
+			"val": 0.0,	# Value 
+			"sth": 0.0	# Stealth 
+		},
+		'§': {
+			"agr": 0.0,	# Aggression 
+			"str": 0.0,	# Strength 
+			"mut": 0.0, # Mutation 
+			"val": 0.0,	# Value 
+			"sth": 2.8	# Stealth 
+		},
+		'^': {
+			"agr": 3.0,	# Aggression 
+			"str": 6.1,	# Strength 
+			"mut": 5.9, # Mutation 
+			"val": 11.6,# Value 
+			"sth": 13.4	# Stealth 
+		},
+		'µ': {
+			"agr": 5.8,	# Aggression 
+			"str": 18.1,# Strength 
+			"mut": 8.2, # Mutation 
+			"val": 11.5,# Value 
+			"sth": 7.3	# Stealth 
+		},
+		'ω': { # UωU 
+			"agr": 9.7,	# Aggression 
+			"str": 12.3,# Strength 
+			"mut": 0.1, # Mutation 
+			"val": 20.0,# Value 
+			"sth": 4.6	# Stealth 
+		},
+		'!': {
+			"agr": 13.2,# Aggression 
+			"str": 14.7,# Strength 
+			"mut": 18.0,# Mutation 
+			"val": 9.4,	# Value 
+			"sth": 0.0	# Stealth 
+		},
+		}
+
+
+# noinspection PyTypeChecker
 class Creature:
 	__slots__ = ["inheritance", "genes"]
 
