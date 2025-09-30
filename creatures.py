74b59509f6cd01b59483d16d3ff874d2749ab5e7
DVP-F <carnx@duck.com>
2025-09-30 14:51:27 +0200

    mayb

--- creatures.py
+++ creatures.py
@@ -107,12 +107,20 @@ class Genes:
 
 # noinspection PyTypeChecker
 class Creature:
-	__slots__ = ["inheritance", "genes", ""]
+	__slots__ = ["inheritance", "genes", "stats"]
 
 	def __init__(self, genes: str = ""):
 		self.inheritance = []
 		self.genes = genes
-		self.mutate()
+		# self.mutate()
+		self.stats = {"agr": 0.0,"str": 0.0,"mut": 0.0,"val": 0.0,"sth": 0.0}
+		for i in range(len(self.genes)):
+			self.stats["agr"] += Genes.genevalues[self.genes[i]]["agr"]
+			self.stats["str"] += Genes.genevalues[self.genes[i]]["str"]
+			self.stats["mut"] += Genes.genevalues[self.genes[i]]["mut"]
+			self.stats["val"] += Genes.genevalues[self.genes[i]]["val"]
+			self.stats["sth"] += Genes.genevalues[self.genes[i]]["sth"]
+		self.stats["mut"] / len(self.genes)
 
 	def mutate(self):
 		return
